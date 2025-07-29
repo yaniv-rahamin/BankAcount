bd65c0ca33d4197ff05b5f12f30740c213b5e86d
yaniv rh <yan1a@hotmail.com>
2025-07-29 19:34:55 +0300

    new BasicAccount

--- BasicAccount.cs
+++ BasicAccount.cs
@@ -6,9 +6,45 @@ using System.Threading.Tasks;
 
 namespace BankAcount
 {
-    internal class BasicAccount
+    public class BasicAccount
     {
-        private int account, bankBranch, bank, id;
-        private double balance, approvedOverdraft;
+        #region fields
+       protected int account, bankBranch, bank, id;
+        protected double balance;
+        #endregion
+
+        #region constructors
+        public BasicAccount(int account, int bankBranch, int bank, int id, double balance)
+        {
+            this.account = account;
+            this.bankBranch = bankBranch;
+            this.bank = bank;
+            this.id = id;
+            this.balance = balance;
+        }
+        #endregion
+
+        #region getters
+        public int GetAccount() { return this.account; }    
+        public int GetBankBranch() { return this.bankBranch; }  
+        public int GetBank() { return this.bank; }
+        public int GetId() { return this.id; }  
+        public double GetBalance() { return this.balance; }
+        #endregion
+
+        #region setters
+        public void SetAccount(int account) { this.account = account; }
+        public void SetBankBranch(int bankBranch) { this.bankBranch = bankBranch; }
+        public void SetBank(int bank) { this.bank = bank; }
+        public void SetId(int id) { this.id = id; }
+        public void SetBalance(double balance) { this.balance = balance; }
+        #endregion
+
+        #region ToString
+        public override string ToString()
+        {
+            return $"Account: {account}, Bank Branch: {bankBranch}, Bank: {bank}, ID: {id}, Balance: {balance}";
+        }
+        #endregion  
     }
 }
